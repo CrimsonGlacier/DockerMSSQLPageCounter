ce3a5981ef1013152ec22dab1f25c936dcae3bd3
CrimsonGlacier <120449326+CrimsonGlacier@users.noreply.github.com>
2023-05-25 20:37:20 -0700

    Add files via upload

--- SQL/run-initialization.sh
+++ SQL/run-initialization.sh
@@ -1,6 +1,5 @@
-# Wait to be sure that SQL Server came up
-sleep 90s
+sleep 10s;
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
-# Run the setup script to create the DB and the schema in the DB
-# Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Onlythestrong99! -d master -i setup.sql
