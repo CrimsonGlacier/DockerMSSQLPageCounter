c5cd66bce36cb16beb36c8d3e6fd771ebe26d3a9
CrimsonGlacier <120449326+CrimsonGlacier@users.noreply.github.com>
2023-06-13 15:36:42 -0700

    Update run-initialization.sh

--- SQL/run-initialization.sh
+++ SQL/run-initialization.sh
@@ -2,4 +2,4 @@ sleep 10s;
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Onlythestrong99! -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P <YOUR_PASSWORD_HERE> -d master -i setup.sql
